ebfd040d7a0ea48cfcf28315e6b334e91ec18b55
Nekro <70316327+NeKroFR@users.noreply.github.com>
2024-08-13 15:38:30 +0200

    Removed useless stuff

--- hash_collider.h
+++ hash_collider.h
@@ -1,15 +1,7 @@
 #ifndef HASH_COLLIDER_H
 #define HASH_COLLIDER_H
 
-#include <getopt.h>
-
-// Default values
 #define WORD_LENGTH 3
 #define HASH_SIZE 3
 
-// Function declarations
-void help();
-void version();
-void parse_arguments(int argc, char *argv[], int *hash_size, int *word_length, char **dict_file, char **algorithm_file);
-
-#endif // HASH_COLLIDER_H
+#endif
